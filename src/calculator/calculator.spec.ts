cca00d34eae60a1295e29c9001cfcae405b7b67b
Milovidov Vasya <milovidov.vasya@gmail.com>
2021-05-16 15:43:51 +0300

    Replace test framework from Mocha to Jest

--- src/calculator/calculator.spec.ts
+++ src/calculator/calculator.spec.ts
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import { Calculator } from '../../src/calculator/Calculator';
+import { Calculator } from './calculator';
 
 /* tslint:disable:no-string-literal */
 describe('Calculator', () => {
